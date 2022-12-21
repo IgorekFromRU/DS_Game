8a23d92bb19d16918f0b3f0c80b4657a1ea5dcb4
IgorekFromRU <igasoft71@gmail.com>
2022-12-21 19:41:28 +0600

    version game v2

--- project_0/game.py
+++ project_0/game.py
@@ -1,13 +1,56 @@
 import numpy as np
-np = np.random.randint(1,101)
-count = 0
-while True:
-    count+=1
-    predicit_number = int(input("Enter number from 1 to 100 to select"))
-    if predicit_number > np:
-        print("Number must be less than", predicit_number,"  ")
-    elif predicit_number < np:
-        print("Number must be greater than", predicit_number,"  ")
-    else:
-        print(f"You have found {predicit_number} for the {count} trying")
-        break
+
+def random_predict(number:int=1) -> int:
+    """Угадываем число за минимальный проход, используя randinit
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
+    count = 0
+    predict_number = 50
+    low_range, high_range  = 1,101
+
+    while True:
+        count += 1
+        
+        if number == predict_number:
+            break # выход из цикла, если угадали        
+        if number < predict_number:
+            high_range = predict_number
+        else:
+            low_range = predict_number
+        
+        # предполагаемое число в сужаемом диапазоне
+        predict_number = np.random.randint(low_range, high_range)
+    return(count)
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+# RUN
+if __name__ == '__main__':
+    score_game(random_predict)
